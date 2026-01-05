03dc6faf41be98afe462dd560983eba915bb33d5
dano2906 <dlg9806@gmail.com>
2026-01-05 15:11:42 -0500

    add shadcn and taliwind

---
+++ app/layouts/default.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full mx-auto container">
+    <slot />
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+  name: "index",
+});
+</script>
+
+<template>
+  <div>index</div>
+</template>
