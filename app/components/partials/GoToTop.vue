7b397909b250010ec008d17b377b50f77a907c41
dano2906 <dlg9806@gmail.com>
2026-01-13 16:34:18 -0500

    fix header sticky position, add go to top and enhance typography

---
+++ app/components/partials/GoToTop.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ChevronUp } from 'lucide-vue-next'
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+
+const isVisible = ref(false)
+
+onMounted(() => {
+  window.addEventListener('scroll', () => {
+    isVisible.value = window.scrollY > 40
+  })
+})
+</script>
+
+<template>
+  <Button class="fixed bottom-6 right-6 p-1 z-50 rounded-full shadow" variant="default" size="icon-lg" :class="[isVisible ? 'block' : 'hidden']" v @click="scrollToTop">
+    <ChevronUp class="size-full" />
+  </Button>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import GoToTop from '@/components/partials/GoToTop.vue'
 import Header from '@/components/partials/Header.vue'
 import { Toaster } from '@/components/ui/sonner'
 import 'vue-sonner/style.css'
@@ -8,6 +9,7 @@ import 'vue-sonner/style.css'
   <div class="w-full mx-auto container max-w-5xl font-inter py-8 px-2 relative">
     <Header />
     <slot />
+    <GoToTop />
   </div>
   <Toaster rich-colors />
 </template>
